da5d67600943f0c20819023cdd8bc892df3bbc36
Christian Nunciato <chris@nunciato.org>
2025-01-08 12:38:43 -0800

    Make the tests pass

--- test_main.py
+++ test_main.py
@@ -4,7 +4,18 @@ from main import generate_pipeline
 
 class TestMain(unittest.TestCase):
     def test_generate_pipeline(self):
-        self.assertEqual(generate_pipeline(), json.dumps({"steps": [{ "command": "echo 'Hello, world!'" }]}, indent=4))
+        self.assertEqual(generate_pipeline(), json.dumps({
+            "steps": [
+                {
+                    "label": ":bazel: Run Bazel build",
+                    "command": "bazel build //:main",
+                },
+                {
+                    "label": ":bazel: Run Bazel tests",
+                    "command": "bazel test //:test_main",
+                },
+            ]
+        }, indent=4))
 
 if __name__ == "__main__":
     unittest.main()
