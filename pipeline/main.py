ab7787d6c35398f077bcf4be5116f3d7f036add3
Christian Nunciato <chris@nunciato.org>
2025-01-08 19:30:00 -0800

    Pipeline labels

--- pipeline/main.py
+++ pipeline/main.py
@@ -5,22 +5,22 @@ def generate_pipeline():
 	pipeline = Pipeline()
 
 	pipeline.add_command_step({
-		"label": "{} Run bazel build //pipeline:all".format(bazel),
+		"label": "{} Build the pipeline binary".format(bazel),
 		"command": "bazel build //pipeline:main",
 	})
 	
 	pipeline.add_command_step({
-		"label": "{} Run Bazel tests".format(bazel),
+		"label": "{} Test the pipeline binary".format(bazel),
 		"command": "bazel test //pipeline:test_main",
 	})
 
 	pipeline.add_command_step({
-		"label": "{} Run Bazel build".format(bazel),
+		"label": "{} Build the emoji library".format(bazel),
 		"command": "bazel build //emojis:emojis",
 	})
 	
 	pipeline.add_command_step({
-		"label": "{} Run Bazel tests".format(bazel),
+		"label": "{} Test the emoji library".format(bazel),
 		"command": "bazel test //emojis:test_emojis",
 	})
 	
