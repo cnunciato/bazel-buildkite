9adc32c1c8fb8deba25a849c1ca7396fd1a4e59b
Christian Nunciato <chris@nunciato.org>
2025-01-11 18:25:47 -0800

    Try uploading

--- pipeline/main.py
+++ pipeline/main.py
@@ -15,11 +15,13 @@ def generate_pipeline():
 	})
 
 	pipeline.add_command_step({
+		"key": "build",
 		"label": "{} Build the emoji library".format(bazel),
 		"command": "bazel build //emojis:emojis",
 	})
 	
 	pipeline.add_command_step({
+		"key": "test",
 		"label": "{} Test the emoji library".format(bazel),
 		"command": "bazel test //emojis:test_emojis",
 	})
@@ -30,10 +32,16 @@ def generate_pipeline():
 	})
 
 	pipeline.add_command_step({
+		"key": "auth",
 		"label": "{}".format(buildkite),
 		"commands": [
 			"BUILDKITE_REGISTRY_TOKEN=\"$(buildkite-agent oidc request-token --audience 'https://packages.buildkite.com/nunciato/bazel-buildkite-emojis' --lifetime 300)\"",
 			"twine upload -u buildkite -p $BUILDKITE_REGISTRY_TOKEN ./bazel-bin/emojis/dist/emojis-0.0.1-py3-none-any.whl"
+		],
+		"depends_on": [
+			"test",
+			"build",
+			"auth",
 		]
 	})
 
