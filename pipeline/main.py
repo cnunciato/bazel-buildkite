48bbf179c51d54c72802c57dac9b8e8d61ae2771
Christian Nunciato <chris@nunciato.org>
2025-03-17 15:42:24 -0700

    Fix API ref

--- pipeline/main.py
+++ pipeline/main.py
@@ -4,29 +4,29 @@ from emojis.emojis import bazel, buildkite
 def generate_pipeline():
 	pipeline = Pipeline()
 
-	pipeline.add_command_step({
+	pipeline.add_step({
 		"label": "{} Build the pipeline binary".format(bazel),
 		"command": "bazel build //pipeline:main",
 	})
 	
-	pipeline.add_command_step({
+	pipeline.add_step({
 		"label": "{} Test the pipeline binary".format(bazel),
 		"command": "bazel test //pipeline:test_main",
 	})
 
-	pipeline.add_command_step({
+	pipeline.add_step({
 		"key": "build",
 		"label": "{} Build the emoji library".format(bazel),
 		"command": "bazel build //emojis:emojis",
 	})
 	
-	pipeline.add_command_step({
+	pipeline.add_step({
 		"key": "test",
 		"label": "{} Test the emoji library".format(bazel),
 		"command": "bazel test //emojis:test_emojis",
 	})
 
-	pipeline.add_command_step({
+	pipeline.add_step({
 		"label": "{} Upload the package".format(buildkite),
 		"commands": [
 			"bazel build //emojis:emojis",
