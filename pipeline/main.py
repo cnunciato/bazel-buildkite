72ac5e86d5c2ccfda0a5060e3bcb2d32b1627b21
Christian Nunciato <chris@nunciato.org>
2025-03-17 16:35:05 -0700

    Update API refs

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,42 +1,42 @@
-from buildkite_sdk import Pipeline
+from buildkite_sdk import Pipeline, CommandStep
 from emojis.emojis import bazel, buildkite
 
 def generate_pipeline():
 	pipeline = Pipeline()
 
-	pipeline.add_step({
-		"label": "{} Build the pipeline binary".format(bazel),
-		"command": "bazel build //pipeline:main",
-	})
+	pipeline.add_step(CommandStep(
+		label="{} Build the pipeline binary".format(bazel),
+		commands="bazel build //pipeline:main",
+	))
 	
-	pipeline.add_step({
-		"label": "{} Test the pipeline binary".format(bazel),
-		"command": "bazel test //pipeline:test_main",
-	})
+	pipeline.add_step(CommandStep(
+		label="{} Test the pipeline binary".format(bazel),
+		commands="bazel test //pipeline:test_main",
+	))
 
-	pipeline.add_step({
-		"key": "build",
-		"label": "{} Build the emoji library".format(bazel),
-		"command": "bazel build //emojis:emojis",
-	})
+	pipeline.add_step(CommandStep(
+		key="build",
+		label="{} Build the emoji library".format(bazel),
+		commands="bazel build //emojis:emojis",
+	))
 	
-	pipeline.add_step({
-		"key": "test",
-		"label": "{} Test the emoji library".format(bazel),
-		"command": "bazel test //emojis:test_emojis",
-	})
+	pipeline.add_step(CommandStep(
+		key="test",
+		label="{} Test the emoji library".format(bazel),
+		commands="bazel test //emojis:test_emojis",
+	))
 
-	pipeline.add_step({
-		"label": "{} Upload the package".format(buildkite),
-		"commands": [
+	pipeline.add_step(CommandStep(
+		label="{} Upload the package".format(buildkite),
+		commands=[
 			"bazel build //emojis:emojis",
 			"curl -X POST https://api.buildkite.com/v2/packages/organizations/nunciato/registries/bazel-buildkite-emojis/packages -H \"Authorization: Bearer $(buildkite-agent oidc request-token --audience 'https://packages.buildkite.com/nunciato/bazel-buildkite-emojis' --lifetime 300)\" -F file=@bazel-bin/emojis/dist/emojis-0.0.2-py3-none-any.whl",
 		],
-		"depends_on": [
+		depends_on=[
 			"test",
 			"build",
 		]
-	})
+	))
 
 	return pipeline.to_json()
 
